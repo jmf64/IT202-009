a1ad45e3cc79853e0b1f0bc8db2aece6f6ead0fe
jmf64 <jmf64@njit.edu>
2020-10-08 10:06:48 -0400

    profile and username

--- project/partials/nav.php
+++ project/partials/nav.php
@@ -4,11 +4,12 @@ require_once(__DIR__ . "/../lib/helpers.php");
 ?>
 <ul>
     <li><a href="home.php">Home</a></li>
-    <?php if(!is_logged_in()):?>
+    <?php if (!is_logged_in()): ?>
         <li><a href="login.php">Login</a></li>
         <li><a href="register.php">Register</a></li>
-    <?php endif;?>
-    <?php if(is_logged_in()):?>
+    <?php endif; ?>
+    <?php if (is_logged_in()): ?>
+        <li><a href="profile.php">Profile</a></li>
         <li><a href="logout.php">Logout</a></li>
     <?php endif; ?>
 </ul>
--- project/register.php
+++ project/register.php
@@ -1,18 +1,11 @@
 <?php require_once(__DIR__ . "/partials/nav.php"); ?>
-<form method="POST">
-    <label for="email">Email:</label>
-    <input type="email" id="email" name="email" required/>
-    <label for="p1">Password:</label>
-    <input type="password" id="p1" name="password" required/>
-    <label for="p2">Confirm Password:</label>
-    <input type="password" id="p2" name="confirm" required/>
-    <input type="submit" name="register" value="Register"/>
-</form>
+
 <?php
 if (isset($_POST["register"])) {
     $email = null;
     $password = null;
     $confirm = null;
+    $username = null;
     if (isset($_POST["email"])) {
         $email = $_POST["email"];
     }
@@ -21,6 +14,9 @@ if (isset($_POST["register"])) {
     }
     if (isset($_POST["confirm"])) {
         $confirm = $_POST["confirm"];
+    }
+    if (isset($_POST["username"])) {
+        $username = $_POST["username"];
     }
     $isValid = true;
     //check if passwords match on the server side
@@ -37,13 +33,13 @@ if (isset($_POST["register"])) {
     //TODO other validation as desired, remember this is the last line of defense
     if ($isValid) {
         $hash = password_hash($password, PASSWORD_BCRYPT);
-        require_once("db.php");
+
         $db = getDB();
         if (isset($db)) {
             //here we'll use placeholders to let PDO map and sanitize our data
-            $stmt = $db->prepare("INSERT INTO Users(email, password) VALUES(:email, :password)");
+            $stmt = $db->prepare("INSERT INTO Users(email, username, password) VALUES(:email,:username, :password)");
             //here's the data map for the parameter to data
-            $params = array(":email" => $email, ":password" => $hash);
+            $params = array(":email" => $email, ":username" => $username, ":password" => $hash);
             $r = $stmt->execute($params);
             //let's just see what's returned
             echo "db returned: " . var_export($r, true);
@@ -52,7 +48,12 @@ if (isset($_POST["register"])) {
                 echo "<br>Welcome! You successfully registered, please login.";
             }
             else {
-                echo "uh oh something went wrong: " . var_export($e, true);
+                if ($e[0] == "23000") {//code for duplicate entry
+                    echo "<br>Either username or email is already registered, please try again";
+                }
+                else {
+                    echo "uh oh something went wrong: " . var_export($e, true);
+                }
             }
         }
     }
@@ -60,4 +61,22 @@ if (isset($_POST["register"])) {
         echo "There was a validation issue";
     }
 }
+//safety measure to prevent php warnings
+if (!isset($email)) {
+    $email = "";
+}
+if (!isset($username)) {
+    $username = "";
+}
 ?>
+<form method="POST">
+    <label for="email">Email:</label>
+    <input type="email" id="email" name="email" required value="<?php safer_echo($email); ?>"/>
+    <label for="user">Username:</label>
+    <input type="text" id="user" name="username" required maxlength="60" value="<?php safer_echo($username); ?>"/>
+    <label for="p1">Password:</label>
+    <input type="password" id="p1" name="password" required/>
+    <label for="p2">Confirm Password:</label>
+    <input type="password" id="p2" name="confirm" required/>
+    <input type="submit" name="register" value="Register"/>
+</form>
